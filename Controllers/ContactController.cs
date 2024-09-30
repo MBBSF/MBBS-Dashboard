260ca74dceb3429e347613fa673b21883988b94d
Younes Abdelkarim <n01491343@unf.edu>
2024-09-30 01:35:43 -0400

    Added 'contact us' page functionality. still needs smtp connection to be fully functional.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using FirstIterationProductRelease.Models;
+
+namespace FirstIterationProductRelease.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SendEmail(ContactDetails model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Configuring the email
+                    var fromAddress = new MailAddress("youremail@example.com", "Your Name");
+                    var toAddress = new MailAddress("destinationemail@example.com");
+                    const string fromPassword = "yourpassword";
+                    string subject = model.Subject;
+                    string body = $"Name: {model.Name}\nEmail: {model.Email}\n\nMessage:\n{model.Message}";
+
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.example.com",  // Change to your SMTP server
+                        Port = 587,  // Change the port according to your server
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                    };
+
+                    using (var message = new MailMessage(fromAddress, toAddress)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(message);
+                    }
+
+                    TempData["Message"] = "Your message has been sent!";
+                    return RedirectToAction("Contact");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, "There was an issue sending your message. Please try again later.");
+                    return View("Contact", model);
+                }
+            }
+
+            // If model is invalid, return to view with errors
+            return View("Contact", model);
+        }
+    }
+}
---
+++ Models/ContactDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstIterationProductRelease.Models
+{
+    public class ContactDetails
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Subject is required")]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(1000, ErrorMessage = "Message is required")]
+        public string Message { get; set; }
+    }
+}
