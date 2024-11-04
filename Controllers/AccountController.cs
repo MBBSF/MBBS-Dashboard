05d647474aeade126e20df432f4c579129d6270c
Abdelrahman Zahra <n01486820@unf.edu>
2024-11-04 11:58:38 -0500

    abdel account edits

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,21 +1,26 @@
 ﻿using FirstIterationProductRelease.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace FirstIterationProductRelease.Controllers
 {
     public class AccountController : Controller
     {
-        private IAccountRepository AccountRepository;
-
+        private readonly IAccountRepository _accountRepository;
+        private readonly IActivityLogRepository _activityLogRepository;
 
         public static Account ActiveAccount;
-        public AccountController(IAccountRepository AccountRepository)
+
+        public AccountController(IAccountRepository accountRepository, IActivityLogRepository activityLogRepository)
         {
-            this.AccountRepository = AccountRepository;
+            _accountRepository = accountRepository;
+            _activityLogRepository = activityLogRepository;
         }
-        
-        public Account GetAccountById(int id) {
-            foreach (Account acc in AccountRepository.Accounts) {
+
+        public Account GetAccountById(int id)
+        {
+            foreach (Account acc in _accountRepository.Accounts)
+            {
                 if (acc.Id == id)
                 {
                     return acc;
@@ -26,7 +31,7 @@ namespace FirstIterationProductRelease.Controllers
 
         public Account GetAccountByUsername(string username)
         {
-            foreach (Account acc in AccountRepository.Accounts)
+            foreach (Account acc in _accountRepository.Accounts)
             {
                 if (acc.Username == username)
                 {
@@ -38,7 +43,6 @@ namespace FirstIterationProductRelease.Controllers
 
         public ActionResult AccountDetails()
         {
-            // simulates data fetching from database
             var accountDetail = new Account
             {
                 LegalName = "Jacob Yoast",
@@ -49,12 +53,13 @@ namespace FirstIterationProductRelease.Controllers
             return View(accountDetail);
         }
 
-
-        public ViewResult AccountList() {
-            return View(AccountRepository.Accounts);
+        public ViewResult AccountList()
+        {
+            return View(_accountRepository.Accounts);
         }
 
-        public ViewResult AccountCreation() {
+        public ViewResult AccountCreation()
+        {
             return View();
         }
 
@@ -63,54 +68,46 @@ namespace FirstIterationProductRelease.Controllers
             return View(GetAccountById(id));
         }
 
-
         [HttpPost]
         public IActionResult Edit(Account acc)
         {
             if (ModelState.IsValid)
             {
-                AccountRepository.SaveAccount(acc);
-                return View("AccountList", AccountRepository.Accounts);
+                _accountRepository.SaveAccount(acc);
+                return View("AccountList", _accountRepository.Accounts);
             }
             else
             {
-                // there is something wrong with the data values
-                return View("AccountSettings",acc);
+                return View("AccountSettings", acc);
             }
         }
 
-        public ViewResult SignIn(Account attempt) {
+        public ViewResult SignIn(Account attempt)
+        {
             Console.WriteLine(attempt.Username + ", " + attempt.Password);
             Account acc = GetAccountByUsername(attempt.Username);
-            if (acc == null || acc.Password != attempt.Password) return View("LogInPage");
-            else ActiveAccount = acc;
+            if (acc == null || acc.Password != attempt.Password)
+                return View("LogInPage");
+            else
+                ActiveAccount = acc;
+
             Console.WriteLine(ActiveAccount.Username + " is the active account");
             return View("Index");
         }
 
-        public ViewResult LogInPage() {
+        public ViewResult LogInPage()
+        {
             return View();
         }
 
         [HttpPost]
         public ViewResult NewAccount(Account acc)
         {
-            if (!ModelState.IsValid) return View("AccountCreation");
-            Random rand = new Random(System.DateTime.Now.Millisecond);
-            AccountRepository.SaveAccount(acc);
-            return View("AccountList", AccountRepository.Accounts);
-        }
+            if (!ModelState.IsValid)
+                return View("AccountCreation");
 
-
-        // log code abdel-
-
-        private IAccountRepository _accountRepository;
-        private IActivityLogRepository _activityLogRepository;
-
-        public AccountController(IAccountRepository accountRepository, IActivityLogRepository activityLogRepository)
-        {
-            _accountRepository = accountRepository;
-            _activityLogRepository = activityLogRepository;
+            _accountRepository.SaveAccount(acc);
+            return View("AccountList", _accountRepository.Accounts);
         }
 
         public ViewResult SignInAttempt(Account attempt)
@@ -131,11 +128,8 @@ namespace FirstIterationProductRelease.Controllers
             var logs = _activityLogRepository.GetLogsForAccount(ActiveAccount.Id);
             return View(logs);
         }
-
-
     }
 
-    // attenpting to create activity log -abdel
     public class ActivityLog
     {
         public int Id { get; set; }
@@ -143,5 +137,4 @@ namespace FirstIterationProductRelease.Controllers
         public string Action { get; set; }
         public DateTime Timestamp { get; set; }
     }
-
 }
