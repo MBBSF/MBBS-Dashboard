2d37bb5d7d15153bd0d9fb1690c6bcd77009bb08
yoasttoast <jacob.yoast@gmail.com>
2025-03-04 15:26:46 -0500

    fixed header issues, one last push for basic filtering

--- Models/DashboardViewModel.cs
+++ Models/DashboardViewModel.cs
@@ -12,6 +12,8 @@ namespace MBBS.Dashboard.web.Models
         public List<ExcelDataCourseraSpecialization> CourseraData { get; set; }
         public List<ExcelDataCognitoMasterList> CognitoData { get; set; }
         public List<ExcelDataGoogleFormsVolunteerProgram> GoogleFormsData { get; set; }
+        public string CurrentSortOrder { get; set; }
+        public string CurrentSortBy { get; set; }
     }
 
     public class KpiData
